bd47b1c696874da27f34fb155034415e27fba521
EletherioAlexandre <eletherioalexandre@gmail.com>
2025-03-31 23:50:29 -0300

    Fixing errors from deploy

--- myApp.js
+++ myApp.js
@@ -45,11 +45,6 @@ const app = express();
 
 
 
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -57,3 +52,5 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Info Security Application started on port ${port}`);
 });
+
+module.exports = app;
